8f5a82f0a968d0d31026a6a76fd500a09ed11140
yulia <ulamuraveva2006@gmail.com>
2023-07-13 23:33:42 +0300

    работает норм

--- app/src/main/java/com/example/fragment/fragments/MapFragment.kt
+++ app/src/main/java/com/example/fragment/fragments/MapFragment.kt
@@ -1,77 +1,178 @@
 package com.example.fragment.fragments
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.viewpager2.widget.ViewPager2
-import com.example.fragment.adapters.ViewPagerAdapter
 import com.example.fragment.R
+import com.example.fragment.adapters.ViewPagerAdapter
+import com.example.fragment.fragments.MapFragment
+import com.example.fragment.fragments.PhoneFragment
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import kotlin.text.toFloatOrNull as toFloatOrNull1
+import java.text.DecimalFormat
+import kotlin.math.roundToInt
 
 @Suppress("UNREACHABLE_CODE")
 class MapFragment : Fragment() {
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private lateinit var text_current_lat_lon: TextView
+    lateinit var locationRequest: com.google.android.gms.location.LocationRequest
+    var current_lat = 0.0f
+    var current_lon = 0.0f
+    var PERMISSION_ID = 52
 
-    var tabTitle = arrayOf("Phone", "Map")
-    lateinit var t: String
-    var a: String? = ""
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        t = arguments?.getString("t").toString()
-        Log.d("Mylog", "map" + t)
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        getLastLocation()
+        Log.d("Mylog", "oncreate")
     }
-
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
         // Inflate the layout for this fragment
         var view: View? = null
         view = inflater.inflate(R.layout.fragment_map, container, false)
-
-
         return view
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        //mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        //var intent = Intent("ru.yandex.yandexnavi.action.BUILD_ROUTE_ON_MAP")
+        //intent.setPackage("ru.yandex.yandexnavi")
+
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        webViewSetup()
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun webViewSetup(){
         var wb_webview = view?.findViewById<WebView>(R.id.wb_webView)
-        var text_current_lat_lon: TextView? = view?.findViewById(R.id.text_current_lat_lon)
-        Log.d("Mylog", t?.split(" ").toString())
-        val c_la: Float? = t?.split(" ")?.get(0)?.toFloatOrNull1()
-        val c_lo: Float? = t?.split(" ")?.get(1)?.toFloatOrNull1()
-        Log.d("Mylog", "${c_la} ${c_lo}")
         wb_webview?.webViewClient = WebViewClient()
         wb_webview?.apply {
-            loadUrl("https://yandex.ru/maps/?rtext=${c_la}, ${c_lo}~55.76009,37.648801&rtt=mt")
-
+            loadUrl("https://yandex.ru/maps/?rtext=${current_lat}, ${current_lon}~55.76009,37.648801&rtt=mt")
             settings.javaScriptEnabled = true
             settings.safeBrowsingEnabled = true
         }
     }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getLastLocation(){
+        if (checkPermission()){
+            mFusedLocationClient.lastLocation.addOnCompleteListener(requireActivity()) { task ->
+                var location: Location? = task.result
+                if (location == null) {
+                    getNewLocation()
+                } else {
+                    current_lat = ((location.latitude.toFloat() * 10000000).roundToInt() / 10000000.0).toFloat()
+                    current_lon = ((location.longitude.toFloat() * 10000000).roundToInt() / 10000000.0).toFloat()
+                    Log.d("Mylog", current_lat.toString() + current_lon.toString())
+                    webViewSetup()
+                }
+            }
 
+        }else{
+            requestPermission()
+        }
+    }
 
+    private fun getNewLocation(){
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        if (ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+    }
+    private val mLocationCallback = object : LocationCallback() {
+        @RequiresApi(Build.VERSION_CODES.O)
+        override fun onLocationResult(locationResult: LocationResult) {
+            var mLastLocation: Location? = locationResult.lastLocation
+            current_lat = mLastLocation!!.latitude.toFloat()
+            current_lon = mLastLocation!!.longitude.toFloat()
+            webViewSetup()
+        }
+    }
 
+    private fun checkPermission(): Boolean{
+        if(ActivityCompat.checkSelfPermission(
+                requireActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                requireActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            return true
+        }
+        return false
+    }
 
+    private fun requestPermission(){
+        ActivityCompat.requestPermissions(requireActivity(), arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_ID)
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_ID){
+            if (grantResults.isEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Log.d("MyLog", "permittion granted")
+            }
+
+        }
+    }
 }
